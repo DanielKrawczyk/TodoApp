960b8195d8b1e041211d6ad531a144b5d26e499a
DanielKrawczyk <80767727+DanielKrawczyk@users.noreply.github.com>
2021-07-10 12:57:32 +0200

    Updates

--- Code/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() { // I think there is no need to explain this function
-    return (
-        <div className="footer">
-            <p>&reg; Copyright 2021 - Daniel Krawczyk</p>
-        </div>
-    )
-}
-
-export default Footer;
